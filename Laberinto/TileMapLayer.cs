bb31d1b6581bc8fb278b8e2054a73f9015e48a44
Javieraa05 <javier.aristigui@gmail.com>
2024-12-24 12:25:09 -0500

    Organice mejor el proyecto

--- Laberinto/TileMapLayer.cs
+++ Laberinto/TileMapLayer.cs
@@ -1,35 +1,35 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using ClassLaberinto;
+
 
 public partial class TileMapLayer : Godot.TileMapLayer
 {
-	Vector2I tile = new Vector2I(0,0);
+    [Export] public int cantidadTrampas = 3;
+    Vector2I tile = new Vector2I(0,0);
     Vector2I position = new Vector2I(0,0);
-	int tile_id = 1;
+    int tile_id = 1;
     Laberinto laberinto = new Laberinto(); 
-	private PackedScene _trapScene1; // Trampa instanciable
-	private PackedScene _trapScene2; // Trampa instanciable
-	private PackedScene _trapScene3; // Trampa instanciable
+    private PackedScene _trapScene1; // Trampa instanciable
+    private PackedScene _trapScene2; // Trampa instanciable
+    private PackedScene _trapScene3; // Trampa instanciable
 
-	public override void _Ready()
-	{
-		 laberinto.InicializarTablero();
-		 laberinto.GenerarLaberintoConPrim();
-		 laberinto.ColocarTrampas(3,3);
-		 laberinto.ColocarTrampas(3,4);
-		 laberinto.ColocarTrampas(3,5);
-		 _trapScene1 = GD.Load<PackedScene>("res://Traps/Trap1.tscn");
-		 _trapScene2 = GD.Load<PackedScene>("res://Traps/Trap2.tscn");
-		 _trapScene3 = GD.Load<PackedScene>("res://Traps/Trap3.tscn");
-		 Mostrar();
-	}
+    public override void _Ready()
+    {
+        laberinto.InicializarTablero();
+        laberinto.GenerarLaberintoConPrim();
+        laberinto.ColocarTrampas(cantidadTrampas, TipoTrampa.Puas);
+        laberinto.ColocarTrampas(cantidadTrampas, TipoTrampa.Fuego);
+        laberinto.ColocarTrampas(cantidadTrampas, TipoTrampa.Osos);
+        _trapScene1 = GD.Load<PackedScene>("res://Traps/TrampaFuego.tscn");
+        _trapScene2 = GD.Load<PackedScene>("res://Traps/TrampaPuas.tscn");
+        _trapScene3 = GD.Load<PackedScene>("res://Traps/TrampaOso.tscn");
+        Mostrar();
+    }
 
-	
-	public override void _Process(double delta)
-	{
-	}
+    public override void _Process(double delta)
+    {
+    }
 
     public void Mostrar()
     {
@@ -37,57 +37,46 @@ public partial class TileMapLayer : Godot.TileMapLayer
         {
             for (int j = 0; j < laberinto.sizeY; j++)
             {
-                if(laberinto.tablero[i,j] == 1) 
-				{
-					tile.X=0;
-					position.X = i;
-			        position.Y = j;
-					SetCell(position,tile_id,tile);
-				}
-				if(laberinto.tablero[i,j] == 0)
-				{
-				tile.X=1;
-				position.X = i;
-			    position.Y = j;
-				SetCell(position,tile_id,tile);
-				}
-				if(laberinto.tablero[i,j] == 3)
-				{
-					tile.X=1;
-					position.X = i;
-				    position.Y = j;
-					SetCell(position,tile_id,tile);
-					// Instanciar y colocar una trampa
-                    var trapInstance = (Trap1)_trapScene1.Instantiate();
-                    trapInstance.Position = MapToLocal(new Vector2I(i, j));
-                    AddChild(trapInstance);
-				}
-				if(laberinto.tablero[i,j] == 4)
-				{
-				   tile.X=1;
-				   position.X = i;
-				   position.Y = j;
-				   SetCell(position,tile_id,tile);
-				   // Instanciar y colocar una trampa
-                   var trapInstance = (Trap2)_trapScene2.Instantiate();
-                   trapInstance.Position = MapToLocal(new Vector2I(i, j));
-                   AddChild(trapInstance);
-				}
-				if(laberinto.tablero[i,j] == 5)
-				{
-				   tile.X=1;
-				   position.X = i;
-				   position.Y = j;
-				   SetCell(position,tile_id,tile);
-				   // Instanciar y colocar una trampa
-                   var trapInstance = (Trap3)_trapScene3.Instantiate();
-                   trapInstance.Position = MapToLocal(new Vector2I(i, j));
-                   AddChild(trapInstance);
-				}
-            }
+                Tile tileData = laberinto.tablero[i, j];
+                if (!tileData.EsCamino) 
+                {
+                    tile.X = 0;
+                    position.X = i;
+                    position.Y = j;
+                    SetCell(position, tile_id, tile);
+                }
+                else 
+                {
+                    tile.X = 1;
+                    position.X = i;
+                    position.Y = j;
+                    SetCell(position, tile_id, tile);
 
+                    if (tileData.Trampa == TipoTrampa.Puas)
+                    {
+                        var trapInstance = (TrampaFuego)_trapScene1.Instantiate();
+                        trapInstance.Position = MapToLocal(new Vector2I(i, j));
+                        AddChild(trapInstance);
+                    }
+                    else if (tileData.Trampa == TipoTrampa.Fuego)
+                    {
+                        var trapInstance = (TrampaPuas)_trapScene2.Instantiate();
+                        trapInstance.Position = MapToLocal(new Vector2I(i, j));
+                        AddChild(trapInstance);
+                    }
+                    else if (tileData.Trampa == TipoTrampa.Osos)
+                    {
+                        var trapInstance = (TrampaOso)_trapScene3.Instantiate();
+                        trapInstance.Position = MapToLocal(new Vector2I(i, j));
+                        AddChild(trapInstance);
+                    }
+                }
+
+               /* if (tileData.TieneLlave)
+                {
+                    // Aquí podrías instanciar una llave si fuera necesario.
+                }*/
+            }
         }
     }
-
-   
-   }
+}
